2b578f1bf75159c9d562e7faf307ed7e7e69532e
grapeofwrath <69535018+grapeofwrath@users.noreply.github.com>
2024-05-18 08:09:00 -0500

    The Great Refactoring
    - changed name to Phortune
    - added fortune-mod compatability and set as default functionality
    - added support for config file in ~/.config/phortune
    - general code cleanup (fixing shadow vars, etc)

--- gotations.go
+++ gotations.go
@@ -1,36 +1,88 @@
 package main
 
 import (
-    "encoding/json"
     "fmt"
+    "log"
     "math/rand"
     "os"
+    "path/filepath"
+    "strings"
     "gopkg.in/yaml.v3"
-
-    "github.com/charmbracelet/log"
 )
 
-type Quote struct {
+type Config struct {
+    Fortune bool `yaml:"fortune"`
+}
+type Phortune struct {
     Text  string
-    Author string
+    Source string
 }
 
 func main() {
-    if len(os.Args) < 2 {log.Fatal("Error missing args: expected JSON or YAML quotes filepath")}
-    data, err := os.ReadFile(os.Args[1])
-        if err != nil {log.Fatal(err)}
-    var validJson = json.Valid([]byte(data))
-    var quotes []Quote
-    if validJson {
-        err = json.Unmarshal(data, &quotes)
-            if err != nil {log.Fatal(err)}
-    } else {
-        err = yaml.Unmarshal(data, &quotes)
-            if err != nil {log.Fatal(err)}
+    var err error
+    var cfg Config
+    var phData []byte
+    var phRand string
+    var ph []string
+    var phPath string
+    fortune := true
+
+    configDir, err := os.UserConfigDir()
+    if err != nil {
+        log.Printf("ERROR: %v",err)
     }
-    var randQuote = quotes[rand.Intn(len(quotes)-0)]
-    fmt.Println(randQuote.Text)
-    if randQuote.Author != "" {
-        fmt.Println(" - " + randQuote.Author)
+    configCheck, err := filepath.Glob(filepath.Join(configDir, "phortune", "config.yaml"))
+    if err != nil {
+        log.Printf("ERROR: %v",err)
     }
+    if len(configCheck) > 0 {
+        cfgData, err := os.ReadFile(configCheck[0])
+        if err != nil {
+            log.Printf("ERROR: %v",err)
+        }
+        err = yaml.Unmarshal(cfgData, &cfg)
+        if err != nil {
+            log.Printf("ERROR: %v",err)
+        }
+        var nil bool
+        if cfg.Fortune != nil {
+            fortune = cfg.Fortune
+        }
+    }
+
+    phPath = filepath.Join(configDir, "phortune", "phortunes")
+    if os.Args[1] != "" {
+        phPath = os.Args[1]
+    }
+    phPathCheck, err := filepath.Glob(phPath)
+    if err != nil {
+        log.Printf("ERROR: %v",err)
+    }
+    if phPathCheck == nil {
+        log.Println("ERROR: missing or incorrect quotes filepath")
+        os.Exit(1)
+    }
+    phData, err = os.ReadFile(phPath)
+    if err != nil {
+        log.Printf("ERROR: %v",err)
+    }
+
+    if fortune == false {
+        var phRand Phortune
+        var ph []Phortune
+        err = yaml.Unmarshal(phData, &ph)
+        if err != nil {
+            log.Printf("ERROR: %v",err)
+        }
+        phRand = ph[rand.Intn(len(ph)-0)]
+        fmt.Println(phRand.Text)
+        if phRand.Source != "" {
+            fmt.Println(" - " + phRand.Source)
+        }
+        os.Exit(0)
+    }
+    sData := string(phData)
+    ph = strings.Split(sData, "%")
+    phRand = ph[rand.Intn(len(ph)-0)]
+    fmt.Println(strings.TrimSpace(phRand))
 }
